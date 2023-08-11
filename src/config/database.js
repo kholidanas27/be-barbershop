7d1f04f719c4b0320827162ed45e6a84a488ad7d
Kholid Anas A <amrulloh30@gmail.com>
2023-08-11 19:52:38 +0700

    changes db connection with sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,20 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const dbPool = mysql.createPool({
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+    dialect: "mysql",
+  }
+);
 
-module.exports = dbPool.promise();
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
